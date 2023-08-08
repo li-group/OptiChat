5b4486e2248bbca0ef357d23d77b087f32849724
GeraltSHEN <100735135+GeraltSHEN@users.noreply.github.com>
2023-08-08 13:11:56 -0400

    add combobox to select gpt_model

--- src/GUI.py
+++ src/GUI.py
@@ -1,14 +1,28 @@
 import os
 import sys
-from PySide6.QtWidgets import (QApplication, QMainWindow, QVBoxLayout, QHBoxLayout, QScrollArea,
+from PySide6.QtWidgets import (QApplication, QMainWindow, QVBoxLayout, QHBoxLayout, QScrollArea, QComboBox,
                                QWidget, QTextEdit, QPushButton, QLineEdit, QFileDialog, QLabel, QGroupBox)
 from PySide6.QtCore import Qt, QTimer, QThread, Signal
 from PySide6.QtGui import QIcon, QKeySequence, QShortcut
 from PySide6.QtGui import QTextCursor, QColor, QBrush
 
 from Util import load_model, extract_component, add_eg, read_iis
-from Util import infer_infeasibility, param_in_const,extract_summary
+from Util import infer_infeasibility, param_in_const, extract_summary
 from Util import get_completion_from_messages_withfn, gpt_function_call
+
+
+class Combobox(QComboBox):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setStyleSheet('''
+            QComboBox {
+                font-size: 12px; 
+                font-weight: bold; 
+                background-color: blue
+                border: 2px solid white;
+                border-radius: 5px;
+            }
+        ''')
 
 
 class BrowseButton(QPushButton):
@@ -118,25 +132,26 @@ class ProcessThread(QThread):
     iis_relation_signal = Signal(str)
     param_names_signal = Signal(list)
 
-    def __init__(self, py_path, ilp_path, model):
+    def __init__(self, py_path, ilp_path, model, gpt_model):
         super().__init__()
         self.py_path = py_path
         self.ilp_path = ilp_path
         self.model = model
+        self.gpt_model = gpt_model
 
     def run(self):
         const_list, param_list, var_list, PYOMO_CODE = extract_component(self.model, self.py_path)
         self.param_names_signal.emit(param_list)
-        summary = extract_summary(var_list, param_list, const_list, PYOMO_CODE)
+        summary = extract_summary(var_list, param_list, const_list, PYOMO_CODE, self.gpt_model)
         self.table_signal.emit(summary)
-        summary_response = add_eg(summary)
+        summary_response = add_eg(summary, self.gpt_model)
         self.summary_signal.emit(summary_response)
 
         const_names, param_names, iis_dict = read_iis(self.ilp_path, self.model)
         iis_relation = param_in_const(iis_dict)
         self.iis_relation_signal.emit(iis_relation)
         # print(const_names, param_names, iis_dict)
-        infeasibility_report = infer_infeasibility(const_names, param_names, summary)
+        infeasibility_report = infer_infeasibility(const_names, param_names, summary, self.gpt_model)
         self.infeasibility_report_signal.emit(infeasibility_report)
 
 
@@ -145,14 +160,15 @@ class ChatThread(QThread):
     fn_message_signal = Signal(str)
     fn_name_signal = Signal(str)
 
-    def __init__(self, chatbot_messages, param_names_aval, model):
+    def __init__(self, chatbot_messages, param_names_aval, model, gpt_model):
         super().__init__()
         self.chatbot_messages = chatbot_messages
         self.param_names_aval = param_names_aval
         self.model = model
+        self.gpt_model = gpt_model
 
     def run(self):
-        response = get_completion_from_messages_withfn(self.chatbot_messages)
+        response = get_completion_from_messages_withfn(self.chatbot_messages, self.gpt_model)
         if "function_call" not in response["choices"][0]["message"]:
             ai_message = response["choices"][0]["message"]["content"]
             self.ai_message_signal.emit(ai_message)
@@ -180,7 +196,7 @@ class ChatThread(QThread):
                                            'Explain why users instruction is invalid and '
                                            'suggest the parameters that the user can try.'}
             self.chatbot_messages.append(expl_message)
-            response = get_completion_from_messages_withfn(self.chatbot_messages)
+            response = get_completion_from_messages_withfn(self.chatbot_messages, self.gpt_model)
             ai_message = response["choices"][0]["message"]["content"]
             self.ai_message_signal.emit(ai_message)
 
@@ -221,8 +237,8 @@ class InfeasibleModelTroubleshooter(QMainWindow):
         self.lbl_model.setText(f"Loading the model...")
         self.model, self.ilp_path = load_model(self.py_path)
 
-        py_path, ilp_path, model = self.py_path, self.ilp_path, self.model
-        self.process_thread = ProcessThread(py_path, ilp_path, model)
+        py_path, ilp_path, model, gpt_model = self.py_path, self.ilp_path, self.model, self.combobox.currentText()
+        self.process_thread = ProcessThread(py_path, ilp_path, model, gpt_model)
 
         self.process_thread.table_signal.connect(self.process_table)
         self.process_thread.summary_signal.connect(self.process_summary)
@@ -281,8 +297,8 @@ class InfeasibleModelTroubleshooter(QMainWindow):
         user_message = self.txt_in.toPlainText()
         self.add_message('user', user_message)
         self.txt_in.clear()
-        chatbot_messages, param_names_aval, model = self.chatbot_messages, self.param_names, self.model
-        self.chat_thread = ChatThread(chatbot_messages, param_names_aval, model)
+        chatbot_messages, param_names_aval, model, gpt_model = self.chatbot_messages, self.param_names, self.model, self.combobox.currentText()
+        self.chat_thread = ChatThread(chatbot_messages, param_names_aval, model, gpt_model)
 
         self.chat_thread.ai_message_signal.connect(self.chat_ai_message)
         self.chat_thread.fn_message_signal.connect(self.chat_fn_message)
@@ -319,7 +335,12 @@ class InfeasibleModelTroubleshooter(QMainWindow):
         self.scroll_area.setWidgetResizable(True)
         group_box = InGroupBox()
 
-        # Browse Button, Model Label, Process Button
+        # Combobox, Browse Button, Model Label, Process Button
+        self.combobox = Combobox()
+        self.combobox.setFixedWidth(180)
+        self.combobox.addItem("gpt-4")
+        self.combobox.addItem("gpt-3.5-turbo-16k")
+        self.combobox.setCurrentIndex(0)
         self.lbl_model = QLabel("Selected File: ", self)
         self.btn_browse = BrowseButton("Browse")
         self.btn_browse.setFixedSize(70, 30)
@@ -352,6 +373,7 @@ class InfeasibleModelTroubleshooter(QMainWindow):
         # Layout
         # Top Layout
         top_layout = QHBoxLayout()
+        top_layout.addWidget(self.combobox)
         top_layout.addWidget(self.btn_browse)
         top_layout.addWidget(self.lbl_model)
         top_layout.addWidget(self.btn_process)
